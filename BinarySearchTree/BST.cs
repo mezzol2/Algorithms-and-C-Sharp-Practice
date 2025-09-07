74f56094b68037efc9eef733958a2f46519a50b5
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2025-09-07 11:56:38 +0200

    Create inner Node class and implement Height method

--- BinarySearchTree/BST.cs
+++ BinarySearchTree/BST.cs
@@ -2,6 +2,38 @@ namespace BinarySearchTree
 {
     class BST
     {
-        
+        private Node? root = null;
+
+        int FindNodeDepth(Node? node)
+        {
+            if (node == null)
+                return -1;
+
+            return 1 + FindNodeDepth(node.Parent);
+        }
+
+        public int TreeHeight()
+        {
+            return Height(root);
+        }
+
+        int Height(Node? node)
+        {
+            if (node == null)
+            {
+                return -1;
+            }
+            return 1 + Math.Max(Height(node.Left),Height(node.Right));
+        }
+
+        private class Node
+        {
+            public int Data { get; set; }
+            public Node? Left { get; set; } = null;
+            public Node? Right { get; set; } = null;
+            public Node? Parent { get; set; } = null;
+
+
+        }
     }   
 }
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
+            //Create new tree
             BST tree = new BST();
+            //Height of an empty tree is -1
+            Console.WriteLine(tree.TreeHeight());
         }
     } 
 }
