f15d451622c72e288be4c220ea6615a0108b1a7f
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2025-09-11 20:26:45 +0200

    Create BinarySearchTreeAVL project

---
+++ BinarySearchTreeAVL/BSTAVL.cs
@@ -0,0 +1,176 @@
+namespace BinarySearchTreeAVL
+{
+    class BSTAVL
+    {
+        private Node? root = null;
+
+        int FindNodeDepth(Node? node)
+        {
+            if (node == null)
+                return -1;
+
+            return 1 + FindNodeDepth(node.Parent);
+        }
+
+        public int TreeHeight()
+        {
+            if (root is not null)
+            {
+                return root.Height;
+            }
+
+            return -1;
+        }
+
+        public void InsertStart(int data)
+        {
+            //initialize insertion at the root
+            root = Insert(root, data);
+        }
+
+        Node Insert(Node? node, int data)
+        {
+            if (node == null)
+            {   //is there is no Node, make one
+                node = new Node(data);
+            }
+            else if (data < node.Data)
+            {   //if the data is less than the data in the node, Insert to the Left
+                node.Left = Insert(node.Left, data);
+            }
+            else if (node.Data < data)
+            {   //if the data is greater than the data in the node, Insert to the Right
+                node.Right = Insert(node.Right, data);
+            }
+
+            //return either this node
+            return node;
+        }
+
+        public bool SearchStart(int data)
+        {   //returns true is a value exists and false if it does not
+            return Search(root,data) != null;
+        }
+
+        //returns the Node with the lowest value on this branch
+        Node FindMinimum(Node node)
+        {
+            //go down the left side the branch until node.Left is null
+            while (node.Left != null)
+            {
+                node = node.Left;
+            }
+
+            return node;
+        }
+
+        Node? Search(Node? node, int data)
+        {
+            Node? res = null;
+            if (node == null)
+            {
+                //return null
+            }
+            else if (node.Data == data)
+            {   //if the data matches, return this node
+                res = node;
+            }
+            else if (data < node.Data)
+            {   //if the data is less than the node, Search the Left side
+                res = Search(node.Left, data);
+            }
+            else if (data > node.Data)
+            {   //if the data is greater than the node, Search the Right side
+                res = Search(node.Right, data);
+            }
+
+            return res;
+        }
+
+        public void Delete(int data)
+        {
+            root = Remove(root, data);
+        }
+
+        Node? Remove(Node? node, int data)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+            if (data < node.Data)
+            {
+                node.Left = Remove(node.Left, data);
+                return node;
+            }
+            if (data > node.Data)
+            {
+                node.Right = Remove(node.Right, data);
+            }
+
+            //node.Data == data
+            if (node.Left == null)
+            {   //if the left side is null, return whatever is on the Right
+                return node.Right;
+            }
+            if (node.Right == null)
+            {   //if the right side is null, return whatever is on the Left
+                return node.Left;
+            }
+
+            //if neither side is null, we will get the lowest value from the right side, and put that in this node
+            Node minRight = FindMinimum(node.Right);
+            node.Data = minRight.Data;
+            //remove the value contined in minRight node from the tree
+            node.Right = Remove(node.Right, minRight.Data);
+            return node;
+        }
+
+        // Node RightRotation(Node z)
+        // {
+        //     Node y = z.Left;
+        //     Node t2 = y.Right;
+        //     y.Right = z;
+        //     z.Left = t2;
+
+        // }
+
+        private class Node
+        {
+            public int Data { get; set; }
+            public Node? Left { get; set; } = null;
+            public Node? Right { get; set; } = null;
+            public Node? Parent { get; set; } = null;
+
+            public int Height { get; set; } = 0;
+
+            public Node(int data)
+            {
+                Data = data;
+            }
+
+            public void SetHeight()
+            {
+                if (Left is null && Right is null)
+                {
+                    Height = 0;
+                    return;
+                }
+
+                if (Left is null)
+                {
+                    Height = 1 + Right.Height;
+                    return;
+                }
+
+                if (Right is null)
+                {
+                    Height = 1 + Left.Height;
+                    return;
+                }
+
+                Height = 1 + Math.Max(Left.Height, Right.Height);
+            }
+        }
+    }   
+}
---
+++ BinarySearchTreeAVL/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace BinarySearchTreeAVL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create new tree
+            BSTAVL tree = new BSTAVL();
+            //Height of an empty tree is -1
+            Console.WriteLine(tree.TreeHeight());
+            //Insert(50)
+            tree.InsertStart(50);
+            //Height should be 0
+            Console.WriteLine(tree.TreeHeight());
+            //Insert 30 and 80
+            tree.InsertStart(30); tree.InsertStart(80);
+            //Height should be 1
+            Console.WriteLine(tree.TreeHeight());
+            //Insert 60
+            tree.InsertStart(60);
+            //Height should be 2
+            Console.WriteLine(tree.TreeHeight());
+            //Search(60) should return True
+            Console.WriteLine(tree.SearchStart(60));
+            //Search(34) should return false
+            Console.WriteLine(tree.SearchStart(34));
+            //Delete(50) will delete the root
+            tree.Delete(50);
+            //Search(50) should return false
+            Console.WriteLine(tree.SearchStart(50));
+            //TreeHeight() should return 1
+            Console.WriteLine(tree.TreeHeight());
+        }
+    } 
+}
