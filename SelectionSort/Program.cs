084903a3349f1c8162942b350c229d0c88a49326
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2025-09-16 15:33:41 +0200

    Implement the Selection Sort algorithm

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        //initialize an array to sort
+        int[] numbers = { 2, 3, 6, 29, 109, 8987, 298, 129, 42, -3, 92, 11 };
+
+        //sort the array
+        SelectionSort(numbers);
+
+        //print the numbers in the array to check
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine(numbers[i]);
+        }
+    }
+
+    static void SelectionSort(int[] numbers)
+    {
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            int lowest = i;
+            for (int j = i + 1; j < numbers.Length; j++)
+            {
+                if (numbers[j] < numbers[lowest])
+                {
+                    lowest = j;
+                }
+            }
+            if (i != lowest)
+            {
+                int temp = numbers[i];
+                numbers[i] = numbers[lowest];
+                numbers[lowest] = temp;
+            }
+        }
+    }
+}
---
+++ SelectionSort/README.md
@@ -0,0 +1,5 @@
+This program implements the Selection Sort method in order to sort an array.
+
+This algorithm sorts by finding the lowest value in the array, placing that value at the front of the array, and then successivly repeating this process for the second lowest, third lowest, etc. until it has gone through every value in the array.
+
+The time complexity of this algorithm is quadratic or O(n^2).
