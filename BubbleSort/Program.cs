c93d9e912daa327fbf8d56e9f2daa08d79c3cacc
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2025-09-16 15:14:02 +0200

    Implement the Bubble Sort algorithm.

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        //initialize an array to sort
+        int[] numbers = { 2, 3, 6, 29, 109, 8987, 298, 129, 42, -3, 92, 11 };
+
+        //sort the array
+        BubbleSort(numbers);
+
+        //print the numbers in the array to check
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine(numbers[i]);
+        }
+    }
+
+    static void BubbleSort(int[] numbers)
+    {
+        for (int i = 0; i <= numbers.Length - 2; i++) {
+            for (int j = 0; j <= numbers.Length - i - 2; j++)
+            {
+                if (numbers[j] > numbers[j + 1])
+                {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }   
+        }
+    }
+}
---
+++ BubbleSort/README.md
@@ -0,0 +1,5 @@
+This program implements the Bubble Sort method in order to sort an array.
+
+The algorithm sorts by going through the array multiple times and comparing elements in the array that are right next to each other.
+
+The time complexity of this algorithm is quadratic or O(n^2).
