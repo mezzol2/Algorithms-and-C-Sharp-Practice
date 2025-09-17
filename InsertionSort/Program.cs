4215a218f775ab150d06773e4a8baf07bbaf5611
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2025-09-17 04:25:19 +0200

    Implement the Insertion Sort algorithm

---
+++ InsertionSort/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        //initialize an array to sort
+        int[] numbers = { 2, 3, 6, 29, 109, 8987, 298, 129, 42, -3, 92, 11 };
+
+        //sort the array
+        InsertionSort(numbers);
+
+        //print the numbers in the array to check
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine(numbers[i]);
+        }
+    }
+
+    static void InsertionSort(int[] numbers)
+    {
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            int j = i;
+            while (j > 0 && numbers[j - 1] > numbers[j])
+            {
+                int temp = numbers[j - 1];
+                numbers[j - 1] = numbers[j];
+                numbers[j] = temp;
+                j--;
+            }
+        }
+    }
+}
---
+++ InsertionSort/README.md
@@ -0,0 +1,5 @@
+This program implements the Insertion Sort method in order to sort an array.
+
+The algorithm sorts by creating a sorted partition on the right side of the array.  The algorithm then inserts additional values into that sorted partition.
+
+The time complexity of this algorithm is quadratic or O(n^2).
